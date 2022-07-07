093d62c860188fdf6f4da29916c6a4905ab32e52
Maruthamalai <58974695+Maruthamalai@users.noreply.github.com>
2022-07-08 05:09:29 +0530

    Add files via upload

--- BatteryState.cpp
+++ BatteryState.cpp
@@ -6,31 +6,9 @@ bool BMS_ChargeRateIsOk(float chargeRate);
 
 bool batteryIsOk(float temperature, float soc, float chargeRate)
 {
-   bool ret = false;
-   if (BMS_ValuesIsInRange(temperature, BMS_TEMPERATURE_MIN, BMS_TEMPERATURE_MAX) && BMS_ValuesIsInRange(soc, BMS_SOC_MIN, BMS_SOC_MAX) && BMS_ChargeRateIsOk(chargeRate))
-   {
-      // if (BMS_ChargeRateIsOk(chargeRate) == false)
-      // {
-      //    cout << "Charge Rate out of range!\n";
-      //    return false;
-      // }
-      // cout << "Temperature is of range!\n";
-      return true;
-   }
-
-   // if (BMS_ValuesIsInRange(soc, BMS_SOC_MIN, BMS_SOC_MAX) == false)
-   // {
-   //    cout << "State of Charge is of range!\n";
-   //    return false;
-   // }
-
-   // if (BMS_ChargeRateIsOk(chargeRate) == false)
-   // {
-   //    cout << "Charge Rate out of range!\n";
-   //    return false;
-   // }
-
-   return ret;
+   return (BMS_ValuesIsInRange(temperature, BMS_TEMPERATURE_MIN, BMS_TEMPERATURE_MAX) && \
+           BMS_ValuesIsInRange(soc, BMS_SOC_MIN, BMS_SOC_MAX) && \
+           BMS_ChargeRateIsOk(chargeRate));
 }
 
 bool BMS_ValuesIsInRange(float value, float min, float max)
